25b1d2113c54ebfbbcf1adcb0620e348794a2217
dmeij <33659751+dmeij@users.noreply.github.com>
2020-04-17 11:17:15 +0200

    Update ShareModule.java

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -4,18 +4,15 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.Arguments;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
-import android.graphics.Bitmap;
-import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 
 
@@ -38,14 +35,17 @@ public class ShareModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void data(Promise promise) {
-      promise.resolve(processIntent());
+      try
+      {
+          promise.resolve(processIntent());
+      }
+      catch (Exception e){}
   }
 
-    public WritableMap processIntent()
+    public WritableMap processIntent() throws Exception
     {
-        WritableMap map             = Arguments.createMap();
-        WritableArray data          = Arguments.createArray();
-        String value                = "";
+        JSONObject map              = new JSONObject();
+        JSONArray data              = new JSONArray();
         String type                 = "";
         String action               = "";
         Activity currentActivity    = getCurrentActivity();
@@ -56,19 +56,17 @@ public class ShareModule extends ReactContextBaseJavaModule {
             action          = intent.getAction();
             type            = intent.getType();
 
-            Log.e("ReactNativeJS", type);
-
             if(type == null)
                 type = "";
 
             if(Intent.ACTION_SEND.equals(action))
             {
                 if("text/plain".equals(type))
-                    data.pushString(intent.getStringExtra(Intent.EXTRA_TEXT));
-                else if ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type))
+                    data.put(intent.getStringExtra(Intent.EXTRA_TEXT));
+                else if("video/*".equals(type) || "video/mpeg".equals(type) || "video/3gp".equals(type) || "video/mp4".equals(type) || "image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type))
                 {
                     Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-                    data.pushString("file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri));
+                    data.put("file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri));
                 }
             }
             else if(Intent.ACTION_SEND_MULTIPLE.equals(action))
@@ -78,14 +76,14 @@ public class ShareModule extends ReactContextBaseJavaModule {
                     ArrayList items = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
 
                     for (int i = 0; i < items.size(); i++)
-                        data.pushString("file://" + RealPathUtil.getRealPathFromURI(currentActivity, (Uri)items.get(i)));
+                        data.put("file://" + RealPathUtil.getRealPathFromURI(currentActivity, (Uri)items.get(i)));
                 }
             }
         }
 
-        map.putString("type", type);
-        map.putArray("data", data);
+        map.put("type", type);
+        map.put("value", data);
 
-        return map;
+        return jsonConvert.jsonToReact(map);
     }
 }
